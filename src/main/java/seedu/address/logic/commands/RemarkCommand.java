46c736422388531dfd7a69a4460661ecd7acdd8e
bmanara <bmanaranus@gmail.com>
2024-09-23 14:44:59 +0800

    Add remark as command

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,4 +1,12 @@
 package seedu.address.logic.commands;
 
-public class RemarkCommand {
+import seedu.address.model.Model;
+
+public class RemarkCommand extends Command{
+    public static final String COMMAND_WORD = "remark";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult("Remark command executed");
+    }
 }
