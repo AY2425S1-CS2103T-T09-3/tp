46efe2f7b7362325ce8500e007b3601944a6a350
sam-theman88 <samuel.lee9525@gmail.com>
2024-09-27 14:50:38 +0800

    Update the unfinished remark command to throw an exception

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,6 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
 public class RemarkCommandTest {
-    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "This has yet to be implemented!";
-
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
